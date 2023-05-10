05cd6c5eefb9fd0c7910009b016f70c409941f68
Android <us1585707@gmail.com>
2023-05-10 15:19:49 +0300

    greeting page

--- app/src/main/java/com/example/vitaliy/GreetingActivity.java
+++ app/src/main/java/com/example/vitaliy/GreetingActivity.java
@@ -5,15 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GreetingActivity extends AppCompatActivity {
 
+    private TextView textViewSecret;
+    private TextView textViewGreeting;
+    private TextView textViewPeoples;
+    private ImageView imageViewGay;
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeting);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textViewSecret = findViewById(R.id.textViewSecret);
+        textViewGreeting = findViewById(R.id.textViewGreeting);
+        textViewPeoples = findViewById(R.id.textViewPeoples);
+        imageViewGay = findViewById(R.id.imageViewGay);
+        imageViewGay.setVisibility(View.GONE);
+        count = 0;
+        textViewSecret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                if (count == 10) {
+                    count = 0;
+                    imageViewGay.setVisibility(View.VISIBLE);
+                    textViewSecret.setVisibility(View.GONE);
+                    textViewGreeting.setVisibility(View.GONE);
+                    textViewPeoples.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     @Override
